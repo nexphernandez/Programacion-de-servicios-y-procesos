e9e02ec325b8335efc5f62595886461056088e45
nexphernandez <nicolaseh3b1112@gmail.com>
2025-10-03 18:36:12 +0100

    Se realiza el ejercicio

--- Lanzar procesos desde java/procesos-servicios/src/main/java/org/formacion/procesos/ProcesosServiciosApplication.java
+++ Lanzar procesos desde java/procesos-servicios/src/main/java/org/formacion/procesos/ProcesosServiciosApplication.java
@@ -1,10 +1,14 @@
 package org.formacion.procesos;
 
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+/**
+ * @author nexphernandez
+ * @version 1.0.0
+ */
 @SpringBootApplication
 public class ProcesosServiciosApplication {
 
@@ -16,10 +20,10 @@ public class ProcesosServiciosApplication {
     CommandLineRunner demo(Procesos procesos) {
         return args -> {
             System.out.println("Iniciando proceso al arrancar la aplicación...");
-
-            procesos.ejecutar();
-
+            procesos.crearFichero();
+            procesos.contarLineas();
             System.out.println("Proceso finalizado.");
         };
     }
+
 }
