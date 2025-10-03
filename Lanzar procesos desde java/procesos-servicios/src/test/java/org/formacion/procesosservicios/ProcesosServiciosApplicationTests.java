8b2d4f1c71cd9ada29ebfbe54cbb0b4f7a023ac0
nexphernandez <nicolaseh3b1112@gmail.com>
2025-10-03 16:39:28 +0100

    Se genera una nueva tarea

---
+++ Lanzar procesos desde java/procesos-servicios/src/test/java/org/formacion/procesosservicios/ProcesosServiciosApplicationTests.java
@@ -0,0 +1,11 @@
+package org.formacion.procesosservicios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesosServiciosApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
